a28369019582d5071aa282600c0f48cc7bc37576
maxpt95 <m.perez.tambasco@gmail.com>
2025-09-24 10:23:30 +0200

    Update main.py to accept a <path_to_book> parameter

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from stats import (
     sort_characters_count,
     CharactersCount,
 )
+import sys
 
 
 def print_sorted_char_count(chars_count: list[CharactersCount]) -> None:
@@ -13,14 +14,19 @@ def print_sorted_char_count(chars_count: list[CharactersCount]) -> None:
 
 
 def main():
-    BOOK_PATH = "books/frankenstein.txt"
-    book_text = get_book_text(BOOK_PATH)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        return sys.exit(1)
+
+    book_path = sys.argv[1]
+
+    book_text = get_book_text(book_path)
     word_count = get_book_word_count(book_text)
     char_count = get_characters_count(book_text)
     sorted_chars_count = sort_characters_count(char_count)
 
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {BOOK_PATH}")
+    print(f"Analyzing book found at {book_path}")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
