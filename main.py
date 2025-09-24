a9e84a51738bf95f697f15e236ba27b9bfbeefd4
maxpt95 <m.perez.tambasco@gmail.com>
2025-09-24 09:05:55 +0200

    Add get_book_text to main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+
+def get_book_text(file_path: str) -> str:
+    with open(file_path) as book:
+        return book.read()
+
+
+main()
