faad528c7beaf389973fb0688c410ab9f80dd2d0
maxpt95 <m.perez.tambasco@gmail.com>
2025-09-24 10:15:57 +0200

    Added book report to main

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
-from stats import get_book_word_count, get_characters_count
+from stats import (
+    get_book_word_count,
+    get_characters_count,
+    sort_characters_count,
+    CharactersCount,
+)
+
+
+def print_sorted_char_count(chars_count: list[CharactersCount]) -> None:
+    for char_count in chars_count:
+        if char_count["char"].isalpha():
+            print(f"{char_count['char']}: {char_count['count']}")
 
 
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+    BOOK_PATH = "books/frankenstein.txt"
+    book_text = get_book_text(BOOK_PATH)
     word_count = get_book_word_count(book_text)
+    char_count = get_characters_count(book_text)
+    sorted_chars_count = sort_characters_count(char_count)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {BOOK_PATH}")
+    print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
-    print(get_characters_count(book_text))
+    print("--------- Character Count -------")
+    print_sorted_char_count(sorted_chars_count)
+    print("============= END ===============")
 
 
 def get_book_text(file_path: str) -> str:
--- stats.py
+++ stats.py
@@ -1,3 +1,11 @@
+from typing import TypedDict
+
+
+class CharactersCount(TypedDict):
+    char: str
+    count: int
+
+
 def get_book_word_count(book_text: str) -> int:
     return len(book_text.split())
 
@@ -11,3 +19,16 @@ def get_characters_count(book_text: str) -> dict[str, int]:
             char_count[c] = 1
 
     return char_count
+
+
+def sort_characters_count(
+    characters_count: dict[str, int],
+) -> list[CharactersCount]:
+    """Sort character count from highest to lowest."""
+    char_count_list = [
+        CharactersCount(char=char, count=count)
+        for char, count in characters_count.items()
+    ]
+    char_count_list.sort(reverse=True, key=lambda items: items["count"])
+
+    return char_count_list
