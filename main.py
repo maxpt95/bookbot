35e89e233ec64f56c853b8ad039e867c007eb2fb
maxpt95 <m.perez.tambasco@gmail.com>
2025-09-24 09:21:38 +0200

    Add get_characters_count to stats.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from stats import get_book_word_count
+from stats import get_book_word_count, get_characters_count
 
 
 def main():
     book_text = get_book_text("./books/frankenstein.txt")
     word_count = get_book_word_count(book_text)
     print(f"Found {word_count} total words")
+    print(get_characters_count(book_text))
 
 
 def get_book_text(file_path: str) -> str:
--- stats.py
+++ stats.py
@@ -1,2 +1,13 @@
 def get_book_word_count(book_text: str) -> int:
     return len(book_text.split())
+
+
+def get_characters_count(book_text: str) -> dict[str, int]:
+    char_count = {}
+    for c in book_text.lower():
+        if char_count.get(c):
+            char_count[c] += 1
+        else:
+            char_count[c] = 1
+
+    return char_count
